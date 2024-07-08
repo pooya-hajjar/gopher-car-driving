054504925cda97ff3ad1aa0b596e1efcc1e2bd19
pooyaHajjar <pooya-hajjar>
2024-07-08 15:10:28 +0330

    feat: Load environment variables during project initialization

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import ()
+import (
+	"github.com/pooya-hajjar/terminal-car-driving/config"
+	"github.com/pooya-hajjar/terminal-car-driving/exception"
+)
 
-func main(){
+var (
+	appConfig config.Config
+)
+
+func init() {
+	conf, err := config.LoadConfig(".")
+	exception.FatalIfError(err)
+	appConfig = conf
+}
+
+func main() {
 
 }
